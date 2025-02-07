adf8028f42fbeddea0af269792b4ed44310cb2e1
NuclearRedeye <github@mneah.com>
2025-02-07 21:34:25 +0000

    feat: add logging middleware

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import { logger } from './middleware/logger.ts';
 const app = express();
 const port = 3000;
+
+app.use(logger);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
