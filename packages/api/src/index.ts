e5850615f69db4a30b020f26b9436a0c18d23978
NuclearRedeye <github@mneah.com>
2025-02-08 14:10:52 +0000

    feat(api): add cors

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+
+// Clients
 import { check as checkDB } from './clients/postgres.ts';
 
+// Middlewares
+import { cors } from 'cors';
 import { logger } from './middleware/logger.ts';
 
+// Routers
 import { default as metricsRouter } from './routers/metrics-router.ts';
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(logger);
 
 app.use('/metrics', metricsRouter);
