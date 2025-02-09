02982a92620e587dbf921610a76230446c18e25a
NuclearRedeye <github@mneah.com>
2025-02-09 12:12:21 +0000

    chore: update database schema

--- .postgres/00.schema.sql
+++ .postgres/00.schema.sql
@@ -5,5 +5,43 @@ CREATE TABLE IF NOT EXISTS freesg.metric (
   name TEXT NOT NULL,
   description TEXT,
   created_at TIMESTAMP default CURRENT_TIMESTAMP,
-  updated_at TIMESTAMP default CURRENT_TIMESTAMP
+  updated_at TIMESTAMP default CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP default NULL
+);
+
+CREATE TABLE IF NOT EXISTS freesg.report (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP default CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP default CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP default NULL
+);
+
+CREATE TABLE IF NOT EXISTS freesg.report_metric (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  report_id uuid NOT NULL references freesg.report(id),
+  metric_id uuid NOT NULL references freesg.metric(id),
+  created_at TIMESTAMP default CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP default CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP default NULL
+);
+
+CREATE TABLE IF NOT EXISTS freesg.submission (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  report_id uuid NOT NULL references freesg.report(id),
+  status TEXT NOT NULL default 'created',
+  created_at TIMESTAMP default CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP default CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP default NULL
+);
+
+CREATE TABLE IF NOT EXISTS freesg.answer (
+  id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  submission_id uuid NOT NULL references freesg.submission(id),
+  metric_id uuid NOT NULL references freesg.metric(id),
+  value TEXT,
+  created_at TIMESTAMP default CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP default CURRENT_TIMESTAMP,
+  deleted_at TIMESTAMP default NULL
 );
